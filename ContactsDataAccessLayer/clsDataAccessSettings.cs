2a233253590d3ea08ce8c5ea21c56181cab40614
BilalOrabi <bilalorabi99@gmail.com>
2025-08-09 02:41:31 +0300

    Add files via upload

---
+++ ContactsDataAccessLayer/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ContactsDataAccessLayer
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=.;Database=ContactsDB;User Id=YOUR_USERNAME;Password=YOUR_PASSWORD;";
+
+
+    }
+}
